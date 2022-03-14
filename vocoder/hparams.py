8f2cb3722d15a85fa5518e71de9362cdf9362140
RuntimeRacer <runtimeracer@gmail.com>
2022-03-14 23:08:30 +0100

    Switched to RAW bits encoder

--- vocoder/hparams.py
+++ vocoder/hparams.py
@@ -21,7 +21,7 @@ mu_law = True                       # Recommended to suppress noise if using raw
 
 
 # WAVERNN / VOCODER --------------------------------------------------------------------------------
-voc_mode = 'MOL'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
+voc_mode = 'RAW'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
 voc_upsample_factors = (5, 5, 8)    # NB - this needs to correctly factorise hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
