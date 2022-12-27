169c6f74970e62baba0b56e9f67391329d7c9389
RuntimeRacer <49956579+RuntimeRacer@users.noreply.github.com>
2022-12-27 19:38:25 +0100

    removing hidden download directives

--- voicefixer/restorer/__init__.py
+++ voicefixer/restorer/__init__.py
@@ -7,38 +7,5 @@
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-9/14/21 12:31 AM   Haohe Liu      1.0         None
+9/14/21 12:28 AM   Haohe Liu      1.0         None
 """
-
-import os
-import torch
-import urllib.request
-
-meta = {
-    "voicefixer_fe": {
-        "path": os.path.join(
-            os.path.expanduser("~"),
-            ".cache/voicefixer/analysis_module/checkpoints/vf.ckpt",
-        ),
-        "url": "https://zenodo.org/record/5600188/files/vf.ckpt?download=1",
-    },
-}
-
-if not os.path.exists(meta["voicefixer_fe"]["path"]):
-    os.makedirs(os.path.dirname(meta["voicefixer_fe"]["path"]), exist_ok=True)
-    print("Downloading the main structure of voicefixer")
-
-    urllib.request.urlretrieve(
-        meta["voicefixer_fe"]["url"], meta["voicefixer_fe"]["path"]
-    )
-    print(
-        "Weights downloaded in: {} Size: {}".format(
-            meta["voicefixer_fe"]["path"],
-            os.path.getsize(meta["voicefixer_fe"]["path"]),
-        )
-    )
-
-    # cmd = "wget "+ meta["voicefixer_fe"]['url'] + " -O " + meta["voicefixer_fe"]['path']
-    # os.system(cmd)
-    # temp = torch.load(meta["voicefixer_fe"]['path'])
-    # torch.save(temp['state_dict'], os.path.join(os.path.expanduser('~'), ".cache/voicefixer/analysis_module/checkpoints/vf.ckpt"))
--- voicefixer/vocoder/__init__.py
+++ voicefixer/vocoder/__init__.py
@@ -7,24 +7,5 @@
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-9/14/21 1:00 AM   Haohe Liu      1.0         None
+9/14/21 12:28 AM   Haohe Liu      1.0         None
 """
-
-import os
-from voicefixer.vocoder.config import Config
-import urllib.request
-
-if not os.path.exists(Config.ckpt):
-    os.makedirs(os.path.dirname(Config.ckpt), exist_ok=True)
-    print("Downloading the weight of neural vocoder: TFGAN")
-    urllib.request.urlretrieve(
-        "https://zenodo.org/record/5469951/files/model.ckpt-1490000_trimed.pt?download=1",
-        Config.ckpt,
-    )
-    print(
-        "Weights downloaded in: {} Size: {}".format(
-            Config.ckpt, os.path.getsize(Config.ckpt)
-        )
-    )
-    # cmd = "wget https://zenodo.org/record/5469951/files/model.ckpt-1490000_trimed.pt?download=1 -O " + Config.ckpt
-    # os.system(cmd)
